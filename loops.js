a42e8f07a9df41ec05787daa87be41cf5937190e
Reinier Blanco <reinierblanco10@gmail.com>
2019-06-06 18:49:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -43,6 +43,18 @@ function doWhileLoop(array) {
 }
 
 
+function doWhileLoop(num) {
+ var i = 0;
 
+ function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+ do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+
+}
 
 
